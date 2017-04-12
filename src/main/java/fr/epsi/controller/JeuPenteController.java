5ccab18159f5dd20feb9bc833f3039b7e4e835c3
Michaël BERTONI <michael@thynkio.com>
2017-04-12 18:42:54 +0200

    @CrossOrigin controller

--- src/main/java/fr/epsi/controller/JeuPenteController.java
+++ src/main/java/fr/epsi/controller/JeuPenteController.java
@@ -5,10 +5,7 @@ import fr.epsi.model.PlayDto;
 import fr.epsi.model.TurnDto;
 import fr.epsi.service.JeuPenteService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -21,6 +18,7 @@ public class JeuPenteController {
     @Autowired
     private JeuPenteService jeuPenteService;
 
+    @CrossOrigin
     @RequestMapping(value = "/connect/{joueurName}", method = RequestMethod.GET)
     public ConnectDto connect(@PathVariable String joueurName, HttpServletResponse response) {
         ConnectDto dto = this.jeuPenteService.connecterJoueur(joueurName);
@@ -28,6 +26,7 @@ public class JeuPenteController {
         return dto;
     }
 
+    @CrossOrigin
     @RequestMapping(value = "/play/{x}/{y}/{idJoueur}", method = RequestMethod.GET)
     public PlayDto play(@PathVariable Integer x, @PathVariable Integer y, @PathVariable String idJoueur, HttpServletResponse response) {
         PlayDto dto = this.jeuPenteService.placerPion(x, y, idJoueur);
@@ -35,6 +34,7 @@ public class JeuPenteController {
         return dto;
     }
 
+    @CrossOrigin
     @RequestMapping(value = "/turn/{idJoueur}", method = RequestMethod.GET)
     public TurnDto turn(@PathVariable String idJoueur, HttpServletResponse response) {
         TurnDto dto = this.jeuPenteService.statusPartieEnCours(idJoueur);
